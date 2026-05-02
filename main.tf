fbeba3d05b5a2f7690cc3db39066d144867d76ec
tbzz83 <tbzz83@gmail.com>
2026-05-02 19:52:47 -0400

    .

--- main.tf
+++ main.tf
@@ -12,6 +12,7 @@ resource "keycloak_openid_client" "openid_client" {
 
     name    = local.client_name
     enabled = true
+    full_scope_allowed = false
 
     access_type           = "CONFIDENTIAL"
     standard_flow_enabled = true
@@ -20,11 +21,30 @@ resource "keycloak_openid_client" "openid_client" {
         "http://localhost:3000/api/openid-callback",
         "http://localhost:3000/api/auth/openid-callback",
         "http://localhost:3000/api/auth/callback",
-
     ]
 
     login_theme = "keycloak"
+    consent_required = true
 
     extra_config = {
     }
+}
+
+resource "keycloak_realm" "realm" {
+  realm   = "my-realm"
+  enabled = true
+}
+
+resource "keycloak_openid_client_scope" "offers_scope" {
+  realm_id               = data.keycloak_realm.realm.id
+  name                   = "offers.get"
+  include_in_token_scope = true
+}
+
+resource "keycloak_openid_client_optional_scopes" "client_optional_scopes" {
+  realm_id  = data.keycloak_realm.realm.id
+  client_id = keycloak_openid_client.openid_client.id
+  optional_scopes = [
+    keycloak_openid_client_scope.offers_scope.name
+  ]
 }
